b30b6bff68156541613bd1c3743f51fd25014552
Brendan <brendan.mckay@gmail.com>
2017-01-30 00:43:56 -0800

    minor tweaks

--- js/page.js
+++ js/page.js
@@ -2,11 +2,9 @@ var resizeAside = function () {
     var aside = document.getElementById("infobox");
     if (window.innerWidth < 768) {
         aside.style.removeProperty('float');
-        aside.style.setProperty('border', '1px #a2a9b1 solid');
     } else {
         aside.style.removeProperty('width');
         aside.style.setProperty('float', 'right');
-        aside.style.setProperty('border', '1px #a2a9b1 solid');
     }
 }
 window.onresize = resizeAside;
