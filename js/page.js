469403034ad2ede8b1011c9f812b84c4bbc67fa7
Brendan <brendan.mckay@gmail.com>
2017-01-31 09:52:24 -0800

    finish layout, prepare for content

--- js/page.js
+++ js/page.js
@@ -1,10 +1,12 @@
 var resizeAside = function () {
     var aside = document.getElementById("infobox");
-    if (window.innerWidth < 768) {
+    if (window.innerWidth < 564) {
         aside.style.removeProperty('float');
+        aside.style.setProperty('margin-left', '-2rem');
     } else {
         aside.style.removeProperty('width');
         aside.style.setProperty('float', 'right');
+        aside.style.setProperty('margin-left', '1rem');
     }
 }
 window.onresize = resizeAside;
